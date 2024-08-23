28763584af76b2bd52798de2f26373941d4ae831
Chethan R <chethan.vasishta@gmail.com>
2024-08-23 07:32:15 +0000

    Moving constants and macros to a common file.

--- source/main.cu
+++ source/main.cu
@@ -1,43 +1,7 @@
+#include "constants.h"
 #include <stdio.h>
 #include <vector>
 #include <string>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <exception>
-#define kNumIterations 10
-
-#define TIMER_EVENTS_CREATE \
-    cudaEvent_t start, stop; \
-    checkCudaErrors(cudaEventCreate(&start)); \
-    checkCudaErrors(cudaEventCreate(&stop));
-
-#define TIMER_EVENTS_RECORD_START \
-    checkCudaErrors(cudaEventRecord(start));
-
-#define TIMER_EVENTS_RECORD_STOP \
-    checkCudaErrors(cudaEventRecord(stop)); \
-    checkCudaErrors(cudaEventSynchronize(stop));
-
-#define TIMER_EVENTS_DESTROY \
-    checkCudaErrors(cudaEventDestroy(start)); \
-    checkCudaErrors(cudaEventDestroy(stop));
-
-#define REPORT_TIME(label) \
-    float milliseconds = 0; \
-    checkCudaErrors(cudaEventElapsedTime(&milliseconds, start, stop)); \
-    milliseconds /= kNumIterations; \
-    printf("Time to %s: %f ms\n", label, milliseconds);
-
-#define checkCudaErrors(ARG)                                                \
-    if (cudaError_t const err = ARG; err != cudaSuccess)                     \
-    {                                                                        \
-        fprintf(stderr, "CUDA Error: %s\n", cudaGetErrorString(err));        \
-        exit(EXIT_FAILURE);                                                  \
-    }
-
-constexpr int kOneM = 1024 * 1024;
-constexpr int kHundredM = 1024 * 1024 * 100;
-constexpr int kOneG = 1024 * 1024 * 1024;
 
 // kernel launch overheads
 
